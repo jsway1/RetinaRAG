e4d5db6e5cf9dc2d0c434d0d8b7a1e9e30213154
jsway1 <45215554+jsway1@users.noreply.github.com>
2024-07-11 13:57:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from scripts.query_RAG import query_rag
 from flask import Flask, request, render_template
 import os
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("RetinaLLM.html")
+
 
 @app.route("/results", methods=["POST"])
 def results():
     query = request.form["query"]
     response = query_rag(query)
     return render_template("RetinaLLM.html", query=query, response=response)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5050)
