cca95e491ca2cabbe100483dc701cb54316f96eb
jsway1 <45215554+jsway1@users.noreply.github.com>
2024-07-11 13:52:38 -0400

    Update createdb.py

--- scripts/createdb.py
+++ scripts/createdb.py
@@ -1,117 +1,118 @@
-# Description: This script is used to create a chroma database for the RetinaLLM project. 
+# Description: This script is used to create a chroma database for the RetinaLLM project.
 
-# Importing required libraries 
-import os 
+# Importing required libraries
+import os
 import argparse
 import shutil
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from langchain.vectorstores.chroma import Chroma
-from langchain_community.embeddings.ollama import LlamafileEmbeddings
+from langchain_community.embeddings import LlamafileEmbeddings
 
-# Defining the paths for the chroma database and location of the PDF files for RAG 
+# Defining the paths for the chroma database and location of the PDF files for RAG
 chroma_path = "CHROMA_PATH"
-data_path = "DATA_PATH"
+data_path = "DATH_PATH"
 
-# Loading the PDF documents 
+# Loading the PDF documents
+
 
 def docloader():
-    
-    '''
+    """
     This function is used to load the PDF documents from the data path.
-    
-    Input: 
-    
+
+    Input:
+
     data_path: str
-    
+
     Returns:
-    
+
     docs: list[Document]
-    '''
-    
+    """
+
     doc_loader = PyPDFDirectoryLoader(data_path)
-    
+
     return doc_loader.load()
+
 
 docs = docloader()
 
 
-# splitting the documents into chunks 
+# splitting the documents into chunks
+
 
 def chunking(docs: list[Document]):
-    
-    '''
-    
+    """
+
     This function is used to split the documents into chunks. After experimentation a chunk size of 1000 with an overlap of 200 was selected as the optimal configuration.
-    
-    
-    Input: 
-    
+
+
+    Input:
+
     docs: list[Document]
-    
+
     Returns:
-    
+
     chunks: list[Document]
-    
-    '''
+
+    """
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
-        chunk_overlap= 200,
+        chunk_overlap=200,
         length_function=len,
         is_separator_regex=False,
     )
-    
+
     return text_splitter.chunking(docs)
+
 
 chunks = chunking(docs)
 
-# creating the function to generate the embeddings 
+# creating the function to generate the embeddings
+
 
 def get_embeddings():
-    
-    '''
-    
-    This function generates the embeddings for the documents. The llamafile is used to generate embeddings.
-    
+    """
+    This function is used to generate the embeddings for the documents. The embedding model used is nomic-embed-text from the langchain community.
+
     Input:
-    
+
     None
-    
+
     Returns:
-    
+
     embeddings
-    
-    '''
-    
+
+    """
+
     embeddings = LlamafileEmbeddings(base_url="http://host.docker.internal:8080")
-    
+
     return embeddings
 
 
-# generate chunk IDs so we can tell which document, page, and chunk the information is from 
+# generate chunk IDs so we can tell which document, page, and chunk the information is from
+
 
 def chunk_ids(chunks):
-    
-    '''
-    
-    This function generates chunk IDs so we can tell which document, page, and chunk the information is from.
-    
-    Input:
-    
-    chunks  (no chunk ID in metadata)
-     
-    Returns:
-    
-    chunks (chunk ID in metadata)
-    
-    '''
+    """
 
-    # initializing last page variable 
-    
+    This function generates chunk IDs so we can tell which document, page, and chunk the information is from.
+
+    Input:
+
+    chunks  (no chunk ID in metadata)
+
+    Returns:
+
+    chunks (chunk ID in metadata)
+
+    """
+
+    # initializing last page variable
+
     last_page = None
-    
-    # setting initial chunk index to 0 
+
+    # setting initial chunk index to 0
     chunk_index = 0
 
     # iterating through the chunks
@@ -120,45 +121,44 @@ def chunk_ids(chunks):
         page = chunk.metadata.get("page")
         current_page = f"{source}:{page}"
 
-        # If we are on the same page, increase chunk index by 1 
+        # If we are on the same page, increase chunk index by 1
         if current_page == last_page:
             chunk_index += 1
         else:
             chunk_index = 0
 
-        # Calculate the chunk ID by combining the page and chunk index 
+        # Calculate the chunk ID by combining the page and chunk index
         chunk_id = f"{current_page}:{chunk_index}"
-        
-        # setting previous page equal to current page 
+
+        # setting previous page equal to current page
         last_page = current_page
 
-        # Add chunk ID to metadata 
+        # Add chunk ID to metadata
         chunk.metadata["id"] = chunk_id
 
     return chunks
 
-# Adding chunks to the Chroma Database 
+
+# Adding chunks to the Chroma Database
+
 
 def populate_chromadb(chunks: list[Document]):
-    
-    '''
-    This function is used to populate the Chroma database with the chunks. If the chunk already exists in the database it will be ignored. 
+    """
+    This function is used to populate the Chroma database with the chunks. If the chunk already exists in the database it will be ignored.
     Only chunks with novel chunk IDs will be added to the database.
-    
+
     Input:
-    
+
     chunks: list[Document]
-    
+
     Returns:
-    
+
     None
-    
-    '''
-    
+
+    """
+
     # Load the existing database.
-    db = Chroma(
-        persist_directory=chroma_path, embedding_function=get_embeddings()
-    )
+    db = Chroma(persist_directory=chroma_path, embedding_function=get_embeddings())
 
     # Calculate Page IDs.
     chunks_with_ids = chunk_ids(chunks)
@@ -173,5 +173,3 @@ def populate_chromadb(chunks: list[Document]):
     for chunk in chunks_with_ids:
         if chunk.metadata["id"] not in existing_ids:
             new_chunks.append(chunk)
-            
-            
